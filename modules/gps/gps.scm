3f32f4e9638ef144504a5f7bd850f7525551bffd
Matthias Görges <mgorges@cw.bc.ca>
2013-05-31 01:42:56 -0700

    MODULES:GPS  Gets physical location from devices (ios and android only)

---
+++ modules/gps/gps.scm
@@ -0,0 +1,76 @@
+#|
+LambdaNative - a cross-platform Scheme framework
+Copyright (c) 2009-2013, University of British Columbia
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or
+without modification, are permitted provided that the
+following conditions are met:
+
+* Redistributions of source code must retain the above
+copyright notice, this list of conditions and the following
+disclaimer.
+
+* Redistributions in binary form must reproduce the above
+copyright notice, this list of conditions and the following
+disclaimer in the documentation and/or other materials
+provided with the distribution.
+
+* Neither the name of the University of British Columbia nor
+the names of its contributors may be used to endorse or
+promote products derived from this software without specific
+prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+|#
+
+;; get physical location from devices (ios and android only)
+
+(c-declare  #<<end-of-c-declare
+
+#ifdef IOS
+  double ios_location_getlatitude(void);
+  double ios_location_getlongitude(void);
+#elif ANDROID
+  double android_location_getlatitude(void);
+  double android_location_getlongitude(void);
+#endif
+
+static double longitude(void){
+#ifdef IOS
+   return ios_location_getlongitude();
+#elif ANDROID
+   return android_location_getlongitude();
+#else
+   return 0;
+#endif
+}
+
+static double latitude(void){
+#ifdef IOS
+   return ios_location_getlatitude();
+#elif ANDROID
+   return android_location_getlatitude();
+#else
+   return 0;
+#endif
+}
+
+end-of-c-declare
+)
+
+(define latitude (c-lambda () double "latitude"))
+(define longitude (c-lambda () double "longitude"))
+;; eof
