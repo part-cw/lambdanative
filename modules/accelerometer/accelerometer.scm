bec170fe7867086c31bba5172e6a62a9d5e249b0
Matthias Görges <mgorges@cw.bc.ca>
2013-05-31 01:44:42 -0700

    MODULES:ACCELEROMETER:  Gets accelerometer data from devices (only android, ios)

---
+++ modules/accelerometer/accelerometer.scm
@@ -0,0 +1,92 @@
+#|
+LambdaNative - a cross-platform Scheme framework
+Copyright (c) 2009-2013, University of British Columbia
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or
+without modification, are permitted provided that the
+following conditions are met:
+
+* Redistributions of source code must retain the above
+copyright notice, this list of conditions and the following
+disclaimer.
+
+* Redistributions in binary form must reproduce the above
+copyright notice, this list of conditions and the following
+disclaimer in the documentation and/or other materials
+provided with the distribution.
+
+* Neither the name of the University of British Columbia nor
+the names of its contributors may be used to endorse or
+promote products derived from this software without specific
+prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+|#
+
+;; get accelerometer data from devices (only android, ios)
+
+(c-declare  #<<end-of-c-declare
+
+#ifdef IOS
+  double ios_accel_getx(void);
+  double ios_accel_gety(void);
+  double ios_accel_getz(void);
+#endif
+
+#ifdef ANDROID
+  double android_accel_getx(void);
+  double android_accel_gety(void);
+  double android_accel_getz(void);
+#endif
+
+static double accel_x(void) {
+#ifdef IOS
+  return ios_accel_getx();
+#elif ANDROID
+  return android_accel_getx();
+#else
+  return 0;
+#endif
+}
+
+static double accel_y(void) {
+#ifdef IOS
+  return ios_accel_gety();
+#elif ANDROID
+  return android_accel_gety();
+#else
+  return 0;
+#endif
+}
+
+static double accel_z(void) {
+#ifdef IOS
+  return ios_accel_getz();
+#elif ANDROID
+  return android_accel_getz();
+#else
+  return 0;
+#endif
+}
+
+end-of-c-declare
+)
+
+(define accel-x (c-lambda () double "accel_x"))
+(define accel-y (c-lambda () double "accel_y"))
+(define accel-z (c-lambda () double "accel_z"))
+
+;; eof
