part-cw/lambdanative

commit 3f32f4e9638ef144504a5f7bd850f7525551bffd

MODULES:GPS  Gets physical location from devices (ios and android only)

modules/gps/ANDROID.java.in
@@ -1,0 +1,35 @@
+ANDROID_IMPORTS="import android.location.Criteria;\
+import android.location.Location;\
+import android.location.LocationListener;\
+import android.location.LocationManager;"
+
+ANDROID_VARIABLES="private LocationManager locationManager;\
+private String provider;"
+
+ANDROID_ONCREATE="locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);\
+Criteria criteria = new Criteria();\
+provider = locationManager.getBestProvider(criteria, false);\
+Location location = locationManager.getLastKnownLocation(provider);"
+
+ANDROID_ONPAUSE="locationManager.removeUpdates(this);"
+
+ANDROID_ONRESUME="locationManager.requestLocationUpdates(provider, 1000, 1, this);"
+
+ANDROID_ACTIVITYADDITIONS="native void gpsEvent(double lat, double lon);\
+@Override\
+public void onLocationChanged(Location location) {\
+  gpsEvent(location.getLatitude(),location.getLongitude());\
+}\
+@Override\
+public void onProviderDisabled(String provider) {\
+}\
+@Override\
+public void onProviderEnabled(String provider) {\
+}\
+@Override\
+public void onStatusChanged(String provider, int status, Bundle extras) {\
+}"
+
+ANDROID_PERMISSIONS="<uses-feature android:name=\"android.location\" />\
+<uses-permission android:name=\"android.permission.ACCESS_FINE_LOCATION\" />\
+<uses-permission android:name=\"android.permission.ACCESS_COARSE_LOCATION\" />"

modules/gps/ANDROID.c.in
@@ -1,0 +1,8 @@
+// FFI for getting GPS data to Scheme
+static double gps_lon, gps_lat;
+void Java_@SYS_ORGTLD@_@SYS_ORGSLD@_@SYS_LOCASEAPPNAME@_@SYS_APPNAME@_gpsEvent(JNIEnv* e, jobject o, jdouble lat, jdouble lon) {
+  gps_lat=(double)lat;
+  gps_lon=(double)lon;
+}
+android_location_getlatitude(){ return gps_lat; }
+android_location_getlongitude(){ return gps_lon; }

modules/gps/gps.scm
@@ -1,0 +1,76 @@
+#|
+LambdaNative - a cross-platform Scheme framework
+Copyright (c) 2009-2013, University of British Columbia
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or
+without modification, are permitted provided that the
+following conditions are met:
+
+* Redistributions of source code must retain the above
+copyright notice, this list of conditions and the following
+disclaimer.
+
+* Redistributions in binary form must reproduce the above
+copyright notice, this list of conditions and the following
+disclaimer in the documentation and/or other materials
+provided with the distribution.
+
+* Neither the name of the University of British Columbia nor
+the names of its contributors may be used to endorse or
+promote products derived from this software without specific
+prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+|#
+
+;; get physical location from devices (ios and android only)
+
+(c-declare  #<<end-of-c-declare
+
+#ifdef IOS
+  double ios_location_getlatitude(void);
+  double ios_location_getlongitude(void);
+#elif ANDROID
+  double android_location_getlatitude(void);
+  double android_location_getlongitude(void);
+#endif
+
+static double longitude(void){
+#ifdef IOS
+   return ios_location_getlongitude();
+#elif ANDROID
+   return android_location_getlongitude();
+#else
+   return 0;
+#endif
+}
+
+static double latitude(void){
+#ifdef IOS
+   return ios_location_getlatitude();
+#elif ANDROID
+   return android_location_getlatitude();
+#else
+   return 0;
+#endif
+}
+
+end-of-c-declare
+)
+
+(define latitude (c-lambda () double "latitude"))
+(define longitude (c-lambda () double "longitude"))
+;; eof

commit 0a2fd19196d12451b73d31f44668756375e0e287

MODULES:GPS  Implementation in #3f32f4e broke android build if this module was not enabled.

bootstraps/android/bootstrap.java.in
@@ -65,7 +65,7 @@ import android.hardware.SensorManager;
 // Imports needed for modules, e.g. gps
 @ANDROID_IMPORTS@
 
-public class @SYS_APPNAME@ extends Activity implements LocationListener, SensorEventListener{
+public class @SYS_APPNAME@ extends Activity implements @ANDROID_IMPLEMENTS@ SensorEventListener{
 //  private GLSurfaceView mGLView;
   Thread thread;  // OpenSL
   private SensorManager mSensorManager;

make.sh
@@ -861,6 +861,7 @@ android)
       . $javaincfile
       ac_java_sedsub=
       ac_java_subst ANDROID_IMPORTS
+      ac_java_subst ANDROID_IMPLEMENTS
       ac_java_subst ANDROID_VARIABLES
       ac_java_subst ANDROID_ONCREATE
       ac_java_subst ANDROID_ONPAUSE
@@ -868,6 +869,7 @@ android)
       ac_java_subst ANDROID_ONSENSORCHANGED
       ac_java_subst ANDROID_ACTIVITYADDITIONS
       sed -i "$ac_java_sedsub" $hookfile
+      ANDROID_IMPLEMENTS=
     fi
   done
   sed -i 's/@ANDROID_[A-Z]*@//' $hookfile

modules/gps/ANDROID.java.in
@@ -3,6 +3,8 @@ import android.location.Location;\
 import android.location.LocationListener;\
 import android.location.LocationManager;"
 
+ANDROID_IMPLEMENTS="LocationListener,"
+
 ANDROID_VARIABLES="private LocationManager locationManager;\
 private String provider;"
 