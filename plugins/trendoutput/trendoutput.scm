part-cw/lambdanative

commit 9158f5a984414291f886a2f9e827283956bd6595

PLUGINS:  Made PLUGIN:init PLUGIN:caseinit PLUGIN:run PLUGIN:caseend and PLUGIN:end private
Plugins are activated using the (make-instance) function, hence no user-accessible functions are provided here.

plugins/trendoutput/trendoutput.scm
@@ -106,11 +106,11 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   ))
 
 ;; plugin hooks
-(define (trendoutput-init store instance)
+(define (trendoutput:init store instance)
   (instance-setvar! store instance "Handle" #f)
   #t)
 
-(define (trendoutput-caseinit store instance)
+(define (trendoutput:caseinit store instance)
   (if (store-ref store "CaseID" #f) (begin
     (let* ((opath (instance-refvar store instance "OutputPath" scheduler:outputpath))
            (casepath (string-append opath (system-pathseparator) (store-ref store "CaseID" #f))))
@@ -121,7 +121,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
     (trendoutput:start store instance)
   )))
 
-(define (trendoutput-run store instance)
+(define (trendoutput:run store instance)
   (if (and (store-ref store "CaseID" #f)
            ;; forget disk space for now
            #t ;; (> (store-ref store "DiskSpace" 0) 0)
@@ -130,20 +130,20 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   )
   #t)
 
-(define (trendoutput-caseend store instance)
+(define (trendoutput:caseend store instance)
   (if (instance-refvar store instance "Handle" #f)
     (trendoutput:run store instance)
     (trendoutput:stop store instance)
   )
   #t)
 
-(define (trendoutput-end store instance)
+(define (trendoutput:end store instance)
   (if (instance-refvar store instance "Handle" #f)
     (trendoutput:stop store instance))
   #t)
 
 ;; register the plugin
-(plugin-register "trendoutput" trendoutput-init trendoutput-caseinit trendoutput-run 
-                 trendoutput-caseend trendoutput-end 'output)
+(plugin-register "trendoutput" trendoutput:init trendoutput:caseinit trendoutput:run 
+                 trendoutput:caseend trendoutput:end 'output)
 
 ;; eof

plugins/waveoutput/waveoutput.scm
@@ -80,11 +80,11 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
       ))
   ))
 
-(define (waveoutput-init store instance)
+(define (waveoutput:init store instance)
   (instance-setvar! store instance "Handle" #f)
   #t)
 
-(define (waveoutput-caseinit store instance)
+(define (waveoutput:caseinit store instance)
   (if (store-ref store "CaseID" #f) (begin
     (let* ((opath (instance-refvar store instance "OutputPath" scheduler:outputpath))
            (casepath (string-append opath (system-pathseparator) (store-ref store "CaseID" #f))))
@@ -95,7 +95,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
     (waveoutput:start store instance))
   ))
 
-(define (waveoutput-run store instance)
+(define (waveoutput:run store instance)
   (if (and (store-ref store "CaseID" #f)
            ;; forget disk space for now
            #t ;; (> (store-ref store "DiskSpace" 0) 0)
@@ -104,16 +104,16 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   )
   #t)
 
-(define (waveoutput-caseend store instance)
+(define (waveoutput:caseend store instance)
   (if (instance-refvar store instance "Handle" #f) (waveoutput:run store instance))
   (waveoutput:stop store instance)
   #t)
 
-(define (waveoutput-end store instance)
+(define (waveoutput:end store instance)
   (if (instance-refvar store instance "Handle" #f) (waveoutput:stop store instance))
   #t)
 
-(plugin-register "waveoutput" waveoutput-init waveoutput-caseinit waveoutput-run 
-                 waveoutput-caseend waveoutput-end 'output)
+(plugin-register "waveoutput" waveoutput:init waveoutput:caseinit waveoutput:run 
+                 waveoutput:caseend waveoutput:end 'output)
 
 ;; eof

plugins/dse/dse.scm
@@ -107,15 +107,15 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   ))
 
 ;; ---- plugin hooks
-(define (dse-init store instance)
+(define (dse:init store instance)
   #t)
 
-(define (dse-caseinit store instance)
+(define (dse:caseinit store instance)
   (instance-setvar! store instance "FactList" '())
   (instance-setvar! store instance "RepeatFactList" '())
 )
 
-(define (dse-run store instance)
+(define (dse:run store instance)
   (define (dse:outerloop continue?)
     (if continue? 
       (dse:outerloop (dse:innerloop (instance-refvar store instance "Rules"))) 
@@ -159,9 +159,9 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   (instance-setvar! store instance "FactList" '())
   #t)
 
-(define (dse-caseend store instance) #t)
-(define (dse-end store instance) #t)
+(define (dse:caseend store instance) #t)
+(define (dse:end store instance) #t)
 
-(plugin-register "dse" dse-init dse-caseinit dse-run dse-caseend dse-end 'dse)
+(plugin-register "dse" dse:init dse:caseinit dse:run dse:caseend dse:end 'dse)
 
 ;; eof

plugins/rawoutput/rawoutput.scm
@@ -68,11 +68,11 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   ))
 
 ;; plugin hooks
-(define (rawoutput-init store instance)
+(define (rawoutput:init store instance)
   (instance-setvar! store instance "Handle" #f)
   #t)
 
-(define (rawoutput-caseinit store instance)
+(define (rawoutput:caseinit store instance)
   (if (store-ref store "CaseID" #f) (begin
     (let* ((opath (instance-refvar store instance "OutputPath" scheduler:outputpath))
            (casepath (string-append opath (system-pathseparator) (store-ref store "CaseID" #f))))
@@ -83,7 +83,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
     (rawoutput:start store instance)
   )))
 
-(define (rawoutput-run store instance)
+(define (rawoutput:run store instance)
   (if (and (store-ref store "CaseID" #f) 
            #t ;;  (> (store-ref store "DiskSpace" 0) 0) ;; forget disk space for now
       )
@@ -91,17 +91,17 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   )
   #t)
 
-(define (rawoutput-caseend store instance)
+(define (rawoutput:caseend store instance)
   (if (instance-refvar store instance "Handle" #f) (rawoutput:run store instance))
   (rawoutput:stop store instance)
   #t)
 
-(define (rawoutput-end store instance)
+(define (rawoutput:end store instance)
   (if (instance-refvar store instance "Handle" #f) (rawoutput:stop store instance))
   #t)
 
 ;; register the plugin
-(plugin-register "rawoutput" rawoutput-init rawoutput-caseinit rawoutput-run 
-                 rawoutput-caseend rawoutput-end 'output)
+(plugin-register "rawoutput" rawoutput:init rawoutput:caseinit rawoutput:run 
+                 rawoutput:caseend rawoutput:end 'output)
 
 ;; eof

commit d004cdd024f9cd25f40a3dfc9a4954073c978cd2

PLUGINS:  Fix of bug introduced in 9158f5a9, which had duplicate pluginname:run functions.

plugins/trendoutput/trendoutput.scm
@@ -121,7 +121,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
     (trendoutput:start store instance)
   )))
 
-(define (trendoutput:run store instance)
+(define (trendoutput:caserun store instance)
   (if (and (store-ref store "CaseID" #f)
            ;; forget disk space for now
            #t ;; (> (store-ref store "DiskSpace" 0) 0)
@@ -143,7 +143,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   #t)
 
 ;; register the plugin
-(plugin-register "trendoutput" trendoutput:init trendoutput:caseinit trendoutput:run 
+(plugin-register "trendoutput" trendoutput:init trendoutput:caseinit trendoutput:caserun 
                  trendoutput:caseend trendoutput:end 'output)
 
 ;; eof

plugins/waveoutput/waveoutput.scm
@@ -95,7 +95,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
     (waveoutput:start store instance))
   ))
 
-(define (waveoutput:run store instance)
+(define (waveoutput:caserun store instance)
   (if (and (store-ref store "CaseID" #f)
            ;; forget disk space for now
            #t ;; (> (store-ref store "DiskSpace" 0) 0)
@@ -113,7 +113,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   (if (instance-refvar store instance "Handle" #f) (waveoutput:stop store instance))
   #t)
 
-(plugin-register "waveoutput" waveoutput:init waveoutput:caseinit waveoutput:run 
+(plugin-register "waveoutput" waveoutput:init waveoutput:caseinit waveoutput:caserun 
                  waveoutput:caseend waveoutput:end 'output)
 
 ;; eof