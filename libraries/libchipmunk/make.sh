e89a25f1a56f6b18a294a2d89a6afac94b93d16e
Chris Petersen <cpetersen@cw.bc.ca>
2014-09-26 12:38:46 -0700

    chipmunk: make QNX compilation work (for blackberry)

--- libraries/libchipmunk/make.sh
+++ libraries/libchipmunk/make.sh
@@ -6,7 +6,7 @@ package_download $PKGURL $PKGHASH
 cd src
 rm *.o 2> /dev/null
 echo " => compiling source.."
-veval "$SYS_CC -Wall -std=gnu99 -DNDEBUG -DCP_USE_DOUBLES -I../include/chipmunk -c *.c"
+veval "$SYS_CC -Wall -Wc,-std=gnu99 -DNDEBUG -DCP_USE_DOUBLES -I../include/chipmunk -c *.c"
 asserterror $? "compilation failed"
 veval "$SYS_AR ru libchipmunk.a *.o"
 asserterror $? "linking failed"
