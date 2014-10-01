84071e69eaf71b57d1d13dcc49fb4dffeeed3d4d
Matthias Görges <mgorges@cw.bc.ca>
2014-09-30 21:24:36 -0700

    LIBCHIPMUNK:  Fix compilation with gcc. Fixes #41

--- libraries/libchipmunk/make.sh
+++ libraries/libchipmunk/make.sh
@@ -3,10 +3,19 @@ PKGURL=https://chipmunk-physics.net/release/ChipmunkLatest.tgz
 PKGHASH=593a15a9032586e56b16d22d84f4f04c1f11a44e
 package_download $PKGURL $PKGHASH
 
+case $SYS_PLATFORM in
+bb10|playbook)
+  EXTRACC=-Wc,-std=gnu99
+;;
+*)
+  EXTRACC=-std=gnu99
+;;
+esac
+
 cd src
 rm *.o 2> /dev/null
 echo " => compiling source.."
-veval "$SYS_CC -Wall -Wc,-std=gnu99 -DNDEBUG -DCP_USE_DOUBLES -I../include/chipmunk -c *.c"
+veval "$SYS_CC -Wall $EXTRACC -DNDEBUG -DCP_USE_DOUBLES -I../include/chipmunk -c *.c"
 asserterror $? "compilation failed"
 veval "$SYS_AR ru libchipmunk.a *.o"
 asserterror $? "linking failed"
