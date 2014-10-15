78622373392157f75b858754e50e604a27c06eee
Chris Petersen <cpetersen@cw.bc.ca>
2014-10-15 12:30:23 -0700

    libzint: add barcode (QR code etc) library, module and example app

---
+++ libraries/libzint/make.sh
@@ -0,0 +1,27 @@
+
+PKGURL=git://git.code.sf.net/p/zint/code
+PKGHASH=
+
+package_download $PKGURL $PKGHASH
+
+package_patch
+
+cd backend
+
+rm *.o 2> /dev/null
+libzint_version=`cat Makefile | grep DZINT_VERSION | cut -f 2- -d "="`
+libzint_srcs=`ls -1 *.c | sed '/dllversion.c/d' | tr '\n' ' '`
+veval "$SYS_CC -I$SYS_PREFIX/include $libzint_version -c $libzint_srcs"
+asserterror $? "compilation failed"
+veval "$SYS_AR ru $SYS_PREFIX/lib/libzint.a *.o"
+asserterror $? "ar failed"
+veval "$SYS_RANLIB $SYS_PREFIX/lib/libzint.a"
+asserterror $? "ranlib failed"
+
+cat zint.h | sed 's/^ZINT_EXTERN//g' > $SYS_PREFIX/include/zint.h
+
+cd ..
+
+package_cleanup
+
+#eof
