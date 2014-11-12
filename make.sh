019fa4d5bc79fcc6542d5c6616b793b27e26707c
Chris Petersen <cpetersen@cw.bc.ca>
2014-11-12 09:48:24 -0800

    make.sh: allow SETUP to reside in satellite directory

--- make.sh
+++ make.sh
@@ -39,7 +39,12 @@
 
 . ./config.cache
 
-. ./SETUP
+. ./scripts/assert.sh
+. ./scripts/locate.sh
+
+setupfile=`locatefile SETUP`
+assertfile "$setupfile"
+. "$setupfile"
 
 evallog=`pwd`"/eval.log"
 
@@ -47,7 +52,6 @@ if [ -f $evallog ]; then
   rm $evallog
 fi
 
-. ./scripts/assert.sh
 . ./scripts/package.sh
 . ./scripts/verbose.sh
 
@@ -184,54 +188,6 @@ newerindir()
     done
   fi
   echo $result
-}
-
-locatetest()
-{
-  here=`pwd`
-  file=
-  dirs=$(echo "$SYS_PATH" | tr ":" "\n")
-  for dir in $dirs; do
-    tmp="$dir/$2"
-    if [ ! "X$tmp" = "X" ]; then
-      if [ $1 $tmp ]; then
-        if [ "X$file" = "X" ]; then
-          file=$tmp
-        else
-          if [ ! "X$3" = "Xsilent" ]; then
-             echo "WARNING: $file shadows $tmp" 1>&2
-          fi
-        fi
-      fi
-    fi 
-  done
-  if [ "X$file" = "X" ]; then
-    if [ ! "X$3" = "Xsilent" ]; then
-      echo "WARNING: [$2] not found" 1>&2
-    fi
-  fi
-  if [ ! -e $file ]; then
-    if [ ! "X$3" = "Xsilent" ]; then
-      echo "WARNING: [$2] not found" 1>&2
-    fi
-  fi
-  cd "$here"
-  echo $file
-}
-
-locatedir()
-{
-  locatetest "-d" $@
-}
-
-locatefile()
-{
-  locatetest "-f" $@
-}
-
-wildcard_dir()
-{
-  find $1 -maxdepth 0 -print |sort -r| head -n 1
 }
 
 #################################
