fefa431707206493c1086c0662b3a5997911308c
clpetersen <cpetersen@cw.bc.ca>
2015-09-10 10:48:24 -0700

    win32: add support for cygwin environment

--- libraries/libz/make.sh
+++ libraries/libz/make.sh
@@ -4,14 +4,14 @@ PKGHASH=50893291621658f355bc5b4d450a8d06a563053d
 
 package_download $PKGURL $PKGHASH
 
-if [ ! $SYS_PLATFORM = win32 ]; then
+if [ ! $SYS_PLATFORM = win32 ] || [ `uname | cut -c 1-6`=CYGWIN ]; then
   NOQUIET=yupgofigure package_configure --static
 else
   PREFIX=`echo "$SYS_STRIP" | sed 's/strip$//;s/ $//'`
   cat win32/Makefile.gcc | sed "s|^PREFIX|PREFIX=$PREFIX#|" > Makefile
 fi
 
-package_make
+package_make 
 INCLUDE_PATH="$SYS_PREFIX/include" LIBRARY_PATH="$SYS_PREFIX/lib" BINARY_PATH="$SYS_PREFIX/bin" package_make install
 package_cleanup
 
