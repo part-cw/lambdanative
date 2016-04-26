021a23fac518df92f011da40385916afdc38a71e
Vincent Toups <vincent.toups@gmail.com>
2016-04-26 11:41:03 -0400

    Removed scran-macros (renamed to global-macros in previous commit).

--- modules/scran/scran-macros.scm
+++
@@ -1,7 +0,0 @@
-;;; Gambit-c requires that macro-code be `included` directly into
-;;; source code that uses it.  Hence, we separate the macro code from
-;;; the main body of the scran so that the main body of the code
-;;; doesn't need to be endlessly recompiled.
-
-(define-macro (define-component name args #!rest body)
-  `(define ,name (component! (lambda ,args ,@body) (symbol->string ',name))))
