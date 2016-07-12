part-cw/lambdanative

commit 33580a3c99baf21ebfcb64e10f7eb9a6643c882d

SCHEDULER: Move instance-related variables to instance table in store
The main reason for this change is to separate regular store data from
data needed to run the plugins. This way one can clone the store data
locally or over the network while running separate plugin instances in
both systems.

modules/scheduler/instance.scm
@@ -1,6 +1,6 @@
 #|
 LambdaNative - a cross-platform Scheme framework
-Copyright (c) 2009-2013, University of British Columbia
+Copyright (c) 2009-2016, University of British Columbia
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or
@@ -37,11 +37,10 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 |#
 
 ;; plug-in instance related
-
 (define (instance-setvar! store instance name val)
-  (store-set! store (string-append instance "#" name) val))
+  (store:instance-set! store (string-append instance "#" name) val))
 (define (instance-refvar store instance name . fback)
-  (apply store-ref (append (list store (string-append instance "#" name)) fback)))
+  (apply store:instance-ref (append (list store (string-append instance "#" name)) fback)))
 
 (define (instance:pluginname store instance)
   (instance-refvar store instance "PluginName" #f))
@@ -68,11 +67,11 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   (apply instance:proc (append (list store instance plugin:end) x)))
 
 (define (make-instance store instance plugin . config)
-  (let ((is (store-ref store "InstanceList" '())))
+  (let ((is (store:instance-ref store "InstanceList" '())))
     (if (not (member instance is))
       (begin
         (log-system (string-append "instance: creating " instance))
-        (store-set! store "InstanceList" (append is (list instance)))
+        (store:instance-set! store "InstanceList" (append is (list instance)))
         (instance-setvar! store instance "PluginName" plugin)
         (instance-setvar! store instance "PluginType" (plugin:type plugin))
         (for-each (lambda (c) (instance-setvar! store instance (car c) (cadr c))) config)
@@ -80,6 +79,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
         (if (scheduler-initialized?) (begin
           (instance:init store instance)
           (if (store-ref store "CaseID" #f) (instance:caseinit store instance))
+          (store:instance-set! store "DispatchStart" (fl+ ##now (random-real)))
         ))
       )
       (log-warning (string-append "instance: " instance " already exists."))
@@ -87,12 +87,12 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   ))
 
 (define (instance-delete store instance)
-  (let ((is (store-ref store "InstanceList" '())))
+  (let ((is (store:instance-ref store "InstanceList" '())))
     (if (and (member instance is) (not (store-ref store "CaseID" #f)))
       (let ((newlist (let loop ((l is)(res '()))
                        (if (fx= (length l) 0) res
                        (loop (cdr l) (append res (if (equal? instance (car l)) '() (list (car l)))))))))
-        (store-set! store "InstanceList" newlist)
+        (store:instance-set! store "InstanceList" newlist)
         (instance:end store instance)
         #t
       )
@@ -101,7 +101,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   ))
 
 (define (instance:all store)
-  (store-ref store "InstanceList" '()))
+  (store:instance-ref store "InstanceList" '()))
 
 (define (instance:allspecific store itype)
   (let loop ((is (instance:all store))(os '()))

modules/scheduler/scheduler.scm
@@ -1,6 +1,6 @@
 #|
 LambdaNative - a cross-platform Scheme framework
-Copyright (c) 2009-2013, University of British Columbia
+Copyright (c) 2009-2016, University of British Columbia
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or
@@ -128,8 +128,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   (let ((timedelta (/ 5.0 (if (fx> (length (store-list)) 0) (length (store-list)) 1.))))
     (let loop ((sl (store-list))(n 0))
       (if (> (length sl) 0) (begin
-        (store-set! (car sl) "DispatchStart" (flo (+ ##now (* timedelta n))))
-        (store-set! (car sl) "DispatchCount" 0.)
+        (store:instance-set! (car sl) "DispatchStart" (flo (+ ##now (* timedelta n))))
+        (store:instance-set! (car sl) "DispatchCount" 0.)
         (loop (cdr sl) (fx+ n 1))
       ))
     )
@@ -163,9 +163,10 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   (if (scheduler:doinputs)
     (begin
       (for-each (lambda (s)
-        (if (fl>= (fl- ##now (store-ref s "DispatchStart" 0.)) (store-ref s "DispatchCount" 0.))  ;; run every 1 second
+        (if (fl>= (fl- ##now (store:instance-ref s "DispatchStart" 0.)) (store:instance-ref s "DispatchCount" 0.))
+          ;; run every 1 second
           (begin
-            (store-set! s "DispatchCount" (fl+ (store-ref s "DispatchCount" 0.) 1.))
+            (store:instance-set! s "DispatchCount" (fl+ (store:instance-ref s "DispatchCount" 0.) 1.))
             (store:waveform-dispatch s)
             (store:raw-dispatch s)
             ;; provide waveform access to gui here

commit 891c6a13864a3f2100ec1006e9317dfc674c3934

SCHEDULER: Fix bug introduced in 33580a3 with DispatchStart
If store exists and a new plugin is added, DispatchStart might
already exist. In this case overwriting DispatchStar will make
plugins sleep until DispatchCount steps are reached. However,
DispatchStart needs to be initialized for new stores created
in the meantime as otherwise we enter loops that run continuously.

modules/scheduler/instance.scm
@@ -79,7 +79,9 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
         (if (scheduler-initialized?) (begin
           (instance:init store instance)
           (if (store-ref store "CaseID" #f) (instance:caseinit store instance))
-          (store:instance-set! store "DispatchStart" (fl+ ##now (random-real)))
+          (if (not (store:instance-ref store "DispatchStart"))
+            (store:instance-set! store "DispatchStart" (fl+ ##now (random-real)))
+          )
         ))
       )
       (log-warning (string-append "instance: " instance " already exists."))