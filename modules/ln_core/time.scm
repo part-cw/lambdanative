part-cw/lambdanative

commit caa135039e0f90e49b4218220c7dca1379a8d257

ln_core: fix timezone handling in string<->seconds

modules/ln_core/time.scm
@@ -46,13 +46,13 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <stdlib.h>
 #include <math.h>
 
-double timezone_hours() {
+double timezone_hours(unsigned long int t) {
   double tz=0;
   long dtime = 0;
   time_t tm1, tm2;
   struct tm *t1, *t2;
   tzset();
-  tm1 = time(NULL);
+  tm1 = (time_t) t;
   t2 = gmtime(&tm1);
   tm2 = mktime(t2);
   t1 = localtime(&tm1);
@@ -68,7 +68,7 @@ double timezone_hours() {
 end-of-c-declare
 )
 
-(define timezone-hours (c-lambda () double "timezone_hours"))
+(define timezone-hours (c-lambda (unsigned-long) double "timezone_hours"))
 
 ;; SRFI-19: Time Data Types and Procedures.
 ;;
@@ -1492,16 +1492,16 @@ end-of-c-declare
         (if (char=? (car cs) #\%) #\~ (car cs))))))))
 
 (define (string->seconds str fmt . tz0)
-  (let* ((tz (if (= (length tz0) 1) (car tz0) (timezone-hours)))
-         (date (string->date str (tm:tildify fmt)))
+  (let* ((date (string->date str (tm:tildify fmt)))
          (t (date->time-monotonic date))
          (s (srfi19:time-second t))
+         (tz (if (= (length tz0) 1) (car tz0) (timezone-hours (fix s))))
          (ns (srfi19:time-nanosecond t))
          (utc (+ 0.0 (* tz -3600.) s (* ns 1.0e-9))))
     (- utc (tm:leap-second-delta utc))))
 
 (define (seconds->string sec0 fmt . tz0)
-  (let* ((tz (if (= (length tz0) 1) (car tz0) (timezone-hours)))
+  (let* ((tz (if (= (length tz0) 1) (car tz0) (timezone-hours (fix sec0))))
          (sec (+ sec0 (* tz 3600.)))
          (s (inexact->exact (floor sec)))
          (ns (inexact->exact (floor (* 1.0e9 (- sec s)))))

commit 9c24ab76535ff7c827b0bb716b46a675b544b311

LN_CORE:  timezone_hours must allow unsigned long inputs.
Fixes issue introduced in caa1350 which crashes for times before
the start of the unix epoch.

modules/ln_core/time.scm
@@ -46,7 +46,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <stdlib.h>
 #include <math.h>
 
-double timezone_hours(unsigned long int t) {
+double timezone_hours(long int t) {
   double tz=0;
   long dtime = 0;
   time_t tm1, tm2;
@@ -68,7 +68,7 @@ double timezone_hours(unsigned long int t) {
 end-of-c-declare
 )
 
-(define timezone-hours (c-lambda (unsigned-long) double "timezone_hours"))
+(define timezone-hours (c-lambda (long) double "timezone_hours"))
 
 ;; SRFI-19: Time Data Types and Procedures.
 ;;