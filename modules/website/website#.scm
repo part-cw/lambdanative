03a2450c70cc14c88435c6973a7191897df24fcb
Chris Petersen <cpetersen@cw.bc.ca>
2017-03-22 08:47:31 -0700

    website: integrate with cgi module and improve error handling

--- modules/website/website#.scm
+++ modules/website/website#.scm
@@ -3,6 +3,7 @@
   website-serve
   website->table
   website-addhook
+  website-catchall
   make-website
 ))
 
--- modules/website/website.scm
+++ modules/website/website.scm
@@ -43,10 +43,16 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 (##include "website#.scm")
 
 (##namespace ("" 
-  log-system file->u8vector pregexp-replace*
-  string-mapconcat string-split sxml->xml 
+  file->u8vector 
   u8vector-compress u8vector-decompress
+  pregexp-replace* sxml->xml
+  string-mapconcat string-split string-downcase
+  exception->string log:exception-handler 
+  log-error log-system
+  make-safe-thread
 ))
+
+(define (string-split-sane a b) (if (or (not (string? a)) (= (string-length a) 0)) '("") (string-split a b)))
 
 (define website:debuglevel 0)
 (define (website:log level . x)
@@ -54,6 +60,11 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 (define website:compress u8vector-compress)
 (define website:decompress u8vector-decompress)
+
+(define (website:safecall x . y)
+  (with-exception-catcher
+    (lambda (e) (log-error "website:safecall : exception: " (exception->string e)) #f)
+    (lambda () (apply x y))))
 
 ;; ------------------
 ;; recursively load a website
@@ -71,7 +82,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
              (website:scan-directory db (string-append path "/" (car fs)))
              (loop (cdr fs))))))
        ((eq? t 'regular)
-         (let ((trimpath (string-append "/" (string-mapconcat (cdr (string-split (pregexp-replace* "//" path "/") #\/)) "/"))))
+         (let ((trimpath (string-append "/" (string-mapconcat (cdr (string-split-sane (pregexp-replace* "//" path "/") #\/)) "/"))))
            (website:log 2 "==> adding " path " [" trimpath "]..")
            (table-set! db trimpath (website:compress (file->u8vector path)))
          )
@@ -104,49 +115,129 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   (let ((accept-port (open-tcp-server (list server-address: "*" port-number: port reuse-address: #t))))
     (let loop () (let ((connection (read accept-port)))
         (if (not (eof-object? connection))
-            (begin (thread-start! (make-thread (lambda () (website:serve db connection) )))
+            (begin (thread-start! (make-safe-thread (lambda () 
+              (current-exception-handler log:exception-handler)
+              (website:serve db connection) )))
      (loop)))))))
 
+(define (website:trim-string s)
+  (if (string? s)
+    (let ((len (string-length s)))
+      (cond
+        ((= len 0) s)
+        ((char=? (string-ref s 0) #\space) (website:trim-string (substring s 1 len)))
+        ((or (char=? (string-ref s (- len 1)) #\space) 
+             (char=? (string-ref s (- len 1)) #\return))
+           (website:trim-string (substring s 0 (- len 1))))
+        (else s))) s))
+
+(define (website:split-header line)
+  (let ((len (string-length line)))
+    (let loop ((i 0))
+      (if (= i len) (list line "")
+        (if (char=? (string-ref line i) #\:)  
+          (list (string-downcase (website:trim-string (substring line 0 i))) (website:trim-string (substring line (+ i 1) len)))
+            (loop (+ i 1)))))))
+
+(define (website:read-header port)
+  (let loop ((headers '()))
+    (let ((line (website:trim-string (read-line port))))
+      (if (not (string? line)) #f
+        (if (= (string-length line) 0) (begin (website:log 4 "website:read-header: " headers) headers)
+          (loop (append headers (list (website:split-header line)))))))))
+
+(define (website:read-content port headers)
+  (let* ((entry (assoc "content-length" headers))
+         (len (if entry (string->number (cadr entry)) #f))
+         (str (if len (make-string len) #f)))
+    (if str (read-substring str 0 len port)) str))
+
+(define (website:build-cgi-environment request headers)
+  (define (cadr-safe s) (if (> (length s) 1) (cadr s) ""))
+  (define (cgi-extract headers http-id cgi-id . fun)
+    (let* ((entry (assoc http-id headers)))
+      (list (list cgi-id (if entry (if (null? fun) (cadr entry) (fun (cadr entry))) "")))))
+  (let* ((r (string-split-sane request #\space))
+         (m (car r))
+         (p (string-split-sane (cadr-safe r) #\?))
+         (pi (car p))
+         (qs (cadr-safe p)))
+  (append
+    (cgi-extract headers "referer" "HTTP_REFERER")
+    (cgi-extract headers "accept" "HTTP_ACCEPT")
+    (cgi-extract headers "accept-language" "HTTP_ACCEPT_LANGUAGE")
+    (cgi-extract headers "accept-encoding" "HTTP_ACCEPT_ENCODING")
+    (cgi-extract headers "user-agent" "HTTP_USER_AGENT")
+    (cgi-extract headers "host" "HTTP_HOST")
+    (cgi-extract headers "cookie" "HTTP_COOKIE")
+    (cgi-extract headers "connection" "HTTP_CONNECTION")
+    (cgi-extract headers "content-length" "CONTENT_LENGTH")
+    (cgi-extract headers "content-type" "CONTENT_TYPE")
+    `(("REQUEST_METHOD" ,m))
+    `(("PATH_INFO" ,pi))
+    `(("QUERY_STRING" ,qs))
+    `(("REQUEST_URI" ,(cadr r)))
+  )))
+
 (define (website:serve db port)
-  (with-exception-catcher (lambda (e) (close-port port) #f)
-    (lambda () (let ((request (read-line port)))
+  (with-exception-catcher (lambda (e) (website:safecall close-port port) #f)
+   (lambda () 
+     (let* ((request (website:trim-string (website:safecall read-line port)))
+            (headers  (website:read-header port))
+            (cgi-env (website:build-cgi-environment request headers)))
        (if (string? request)
-         (let* ((r (string-split request #\space))
+         (let* ((r (string-split-sane request #\space))
                 (m (car r)))
-            (website:log 3 "website:serve got " request)
-            (if (string-ci=? m "GET")
+            (website:log 3 "website:serve request: " request)
+            (website:log 3 "website:serve header: " headers)
+            (website:log 3 "website:serve cgi env: " cgi-env)
+            (if (or (string-ci=? m "GET") (string-ci=? m "POST"))
               (website:servefile db port
-                (if (string=? (cadr r) "/") "/index.html" (cadr r))))
-            (force-output port)
-            (close-port port)
+                (if (string=? (cadr r) "/") "/index.html" (cadr r)) cgi-env))
+            (website:safecall force-output port)
+            (website:safecall close-port port)
     ))))))
 
-(define (website:servefile db port doc)
+(define (website:servefile db port doc cgi-env)
   (website:log 3 "serving " doc "..")
-  (let* ((decomposit (string-split doc #\?))
+  (let* ((decomposit (string-split-sane doc #\?))
          (document (car decomposit))
          (zdata (table-ref db document #f))
+         (catchall (table-ref db 'catchall #f))
          (data (if (u8vector? zdata) (website:decompress zdata)
-                 (if (procedure? zdata) (zdata (if (> (length decomposit) 1) (cadr decomposit) #f)) #f)))
-         (ext (car (reverse (string-split document #\.)))))
+                 (if (procedure? zdata) (with-input-from-port port (lambda () (zdata cgi-env))) 
+                   (if (procedure? catchall) (with-input-from-port port (lambda () (catchall document cgi-env))) #f))))
+         (ext (car (reverse (string-split-sane document #\.))))
+         (headers (string-append "HTTP/1.0 200 OK\n"
+                                "Content-Type: " (website:lookup-mimetype ext) "\n"
+                                (if (procedure? zdata) "Cache-Control: no-cache, no-store\n" "")
+                              ;;  "Access-Control-Allow-Origin: *\n"
+                              ;;  "Access-Control-Allow-Methods: GET,POST,PUT\n"
+                                "\n"))
+         (catchall (table-ref db 'catchall #f)))
     (if data
       (begin
-        (display (string-append "HTTP/1.0 200 OK\ncontent-type: " (website:lookup-mimetype ext) "\n\n") port)
+        (website:safecall display headers port)
         (if (string=? ext "sxml")
           (sxml->xml (if (= (u8vector-ref data 0) 96) (eval (with-input-from-u8vector data read)) (with-input-from-u8vector data read)) port)
-          (write-subu8vector data 0 (u8vector-length data) port)
+          (website:safecall write-subu8vector data 0 (u8vector-length data) port)
         ))
       (begin
         (website:log 0 "!! unknown source: " document)
-        (display "HTTP/1.0 400 OK\ncontent-type: text/plain\n\nInvalid Request\n" port))
+        (website:safecall display "HTTP/1.0 400 OK\ncontent-type: text/plain\n\nInvalid Request\n" port))
    )))
 
 ;; ----------------
 
-(define (website-serve db port) (thread-start! (make-thread (lambda () (website:server db port)))))
+(define (website-serve db port) (thread-start! (make-safe-thread (lambda () 
+  (current-exception-handler log:exception-handler)
+  (website:server db port)))))
 
 (define (website-addhook db document proc)
   (table-set! db document proc))
+
+(define (website-catchall db proc)
+  (table-set! db 'catchall proc))
 
 (define (website->table path)
   (let ((t (make-table)))
