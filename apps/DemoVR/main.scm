4f43595b39a0894b6ada33aa7568d52a225a16b4
Chris Petersen <cpetersen@cw.bc.ca>
2017-04-27 11:51:11 -0700

    DemoVR: add hybrid virtual reality demo

---
+++ apps/DemoVR/main.scm
@@ -0,0 +1,57 @@
+#|
+LambdaNative - a cross-platform Scheme framework
+Copyright (c) 2009-2017, University of British Columbia
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or
+without modification, are permitted provided that the
+following conditions are met:
+
+* Redistributions of source code must retain the above
+copyright notice, this list of conditions and the following
+disclaimer.
+
+* Redistributions in binary form must reproduce the above
+copyright notice, this list of conditions and the following
+disclaimer in the documentation and/or other materials
+provided with the distribution.
+
+* Neither the name of the University of British Columbia nor
+the names of its contributors may be used to endorse or
+promote products derived from this software without specific
+prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+|#
+
+;; minimal example VR application
+
+;; the 360 degree image is by Ansgar Koreng / CC BY-SA 4.0 (unchanged)
+
+(include "assets.scm")
+
+(define index.sxml
+  `(html (head (script (@ (src "js/aframe.min.js")) ""))
+     (body (a-scene 
+       (a-camera (@ (position "0 2 4")) "")
+       (a-sky (@ (src "img/scene.jpg")) "")))))
+
+(website-addhook #f "/index.html" (lambda (cgi-env)
+  (string->u8vector (string-append "<!DOCTYPE html>"
+    (with-output-to-string "" (lambda () (sxml->xml index.sxml)))))))
+
+(website-serve #f 8080)
+
+;; eof
