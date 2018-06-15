f347cfe7a17e534aac0ab775d61c5b6bf98549a8
Jonathan Chan <afnl686@gmail.com>
2018-06-15 15:40:11 -0700

    LN_CORE: Added string-format function to string.scm.

--- modules/ln_core/string.scm
+++ modules/ln_core/string.scm
@@ -284,5 +284,37 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
         (loop (fx- i 1) (fx- j 1)))
       (else #f))))
 
+;; https://srfi.schemers.org/srfi-28/srfi-28.html
+(define (string-format format-string . objects)
+  (let ((buffer (open-output-string)))
+    (let loop ((format-list (string->list format-string))
+               (objects objects))
+      (cond ((null? format-list) (get-output-string buffer))
+            ((char=? (car format-list) #\~)
+             (if (null? (cdr format-list))
+                 (error 'format "Incomplete escape sequence")
+                 (case (cadr format-list)
+                    ((#\a)
+                     (if (null? objects)
+                         (error 'format "No value for escape sequence")
+                         (begin
+                           (display (car objects) buffer)
+                           (loop (cddr format-list) (cdr objects)))))
+	                  ((#\s)
+                     (if (null? objects)
+                         (error 'format "No value for escape sequence")
+                         (begin
+                           (write (car objects) buffer)
+                           (loop (cddr format-list) (cdr objects)))))
+                    ((#\%)
+                     (newline buffer)
+                     (loop (cddr format-list) objects))
+                    ((#\~)
+                     (write-char #\~ buffer)
+                     (loop (cddr format-list) objects))
+                    (else
+                     (error 'format "Unrecognized escape sequence")))))
+            (else (write-char (car format-list) buffer)
+                  (loop (cdr format-list) objects))))))
 
 ;; eof
