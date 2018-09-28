part-cw/lambdanative

commit c5a10babbe367cd9be6cb7356ffa95e1c9adce05

LOADERS: Added onKeyUp event handling in Android to send EVENT_KEYRELEASE events LN_GLGUI: Added hiding keypad on EVENT_BACK key UIFORM: Added checking that mx is actually a character (as opposed to other special keys) before converting to a character

modules/ln_glgui/keypad.scm
@@ -273,7 +273,10 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
       ((and (fx= type EVENT_MOTION) (glgui-widget-get g wgt 'highlight) armed)
         (let ((keycmd (if key (keypad:keycmd key) #f)))
           (if (and (char? keycmd) (not (char=? keycmd #\nul))) (glgui-widget-set! g wgt 'highlight keycmd))
-        )))
+        ))
+      ((and (fx= type EVENT_KEYRELEASE) (fx= mx EVENT_KEYBACK))
+        (glgui-widget-set! g wgt 'hidden #t)
+        (if (procedure? hideonreturn) (hideonreturn))))
    ;; Return the key, but if false, return whether event was from inside rectangle dimensions
    (if key key inside)
  ))

loaders/android/bootstrap.java.in
@@ -218,11 +218,17 @@ class  xGLSurfaceView extends GLSurfaceView {
     return true;
   }
   public boolean onKeyDown(int keyCode, KeyEvent event) {
+    return onKeyEvent(keyCode, event, 2); // type == EVENT_KEYPRESS == 2
+  }
+  public boolean onKeyUp(int keyCode, KeyEvent event) {
+    return onKeyEvent(keyCode, event, 3); // type == EVENT_KEYRELEASE == 3
+  }
+  private boolean onKeyEvent(int keyCode, KeyEvent event, int type) {
     t=x=y=0;
     // Get known keys
     int asciiKey = event.getUnicodeChar(event.getMetaState());
     if (asciiKey > 0) {
-      t=2;
+      t=type;
       x=asciiKey;
       queueEvent(new Runnable(){ public void run() {
         renderer.nativeEvent(t,x,y); }});
@@ -230,18 +236,18 @@ class  xGLSurfaceView extends GLSurfaceView {
     }
     // Get everything else
     switch (keyCode) {
-      case KeyEvent.KEYCODE_DEL: t=2; x=3; break;
-      case KeyEvent.KEYCODE_ENTER: t=2; x=1; break;
-      case KeyEvent.KEYCODE_DPAD_CENTER: t=2; x=1; break;
-      case KeyEvent.KEYCODE_DPAD_UP: t=2; x=6; break;
-      case KeyEvent.KEYCODE_DPAD_DOWN: t=2; x=7; break;
-      case KeyEvent.KEYCODE_DPAD_LEFT: t=2; x=5; break;
-      case KeyEvent.KEYCODE_DPAD_RIGHT: t=2; x=4; break;
-      case KeyEvent.KEYCODE_BACK: t=2; x=10; break;
-      case KeyEvent.KEYCODE_MENU: t=2; x=9; break;
+      case KeyEvent.KEYCODE_DEL:         t=type; x=3;  break;
+      case KeyEvent.KEYCODE_ENTER:       t=type; x=1;  break;
+      case KeyEvent.KEYCODE_DPAD_CENTER: t=type; x=1;  break;
+      case KeyEvent.KEYCODE_DPAD_UP:     t=type; x=6;  break;
+      case KeyEvent.KEYCODE_DPAD_DOWN:   t=type; x=7;  break;
+      case KeyEvent.KEYCODE_DPAD_LEFT:   t=type; x=5;  break;
+      case KeyEvent.KEYCODE_DPAD_RIGHT:  t=type; x=4;  break;
+      case KeyEvent.KEYCODE_BACK:        t=type; x=10; break;
+      case KeyEvent.KEYCODE_MENU:        t=type; x=9;  break;
       case KeyEvent.KEYCODE_VOLUME_DOWN: return false;
-      case KeyEvent.KEYCODE_VOLUME_UP: return false;
-      case KeyEvent.KEYCODE_SOFT_LEFT: return true;
+      case KeyEvent.KEYCODE_VOLUME_UP:   return false;
+      case KeyEvent.KEYCODE_SOFT_LEFT:   return true;
     }
     if (t>0) {
       queueEvent(new Runnable(){ public void run() {

modules/uiform/uiform.scm
@@ -1678,7 +1678,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
                         (newstr (substring oldstr 0 (- oldstrlen 1))))
                     (xxset focuslocation focusid (substring oldstr 0 (- oldstrlen 1)))
                     (if cb (cb focuslocation focusid newstr)))))
-              (else
+              ((fx> mx 31)
                 (let ((cb (uiget 'focuskeycb #f))
                         (newstr (string-append oldstr (string (integer->char mx)))))
                     (xxset focuslocation focusid newstr)

commit b36f25a91ad47f94eb3a000bc6c3c6ba09a66c36

LN_GLGUI: Fix hideonreturn bug introduced in c5a10ba. Closes #214

modules/ln_glgui/keypad.scm
@@ -274,7 +274,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
         (let ((keycmd (if key (keypad:keycmd key) #f)))
           (if (and (char? keycmd) (not (char=? keycmd #\nul))) (glgui-widget-set! g wgt 'highlight keycmd))
         ))
-      ((and (fx= type EVENT_KEYRELEASE) (fx= mx EVENT_KEYBACK))
+      ((and (fx= type EVENT_KEYRELEASE) (fx= mx EVENT_KEYBACK) hideonreturn)
         (glgui-widget-set! g wgt 'hidden #t)
         (if (procedure? hideonreturn) (hideonreturn))))
    ;; Return the key, but if false, return whether event was from inside rectangle dimensions