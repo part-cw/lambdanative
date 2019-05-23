490038a2007aec0f5957d07302f4a9a4d41d5116
Matthias Görges <mgorges@cw.bc.ca>
2019-05-23 12:31:18 -0700

    LIBGAMBIT: Fix a few small things in e2353ab, drop fixed version

--- languages/scm.c
+++ languages/scm.c
@@ -9,22 +9,18 @@
 #endif
 
 /*
- * Maybe we better fist #include <gambit.h> and then check that we've
+ * Maybe we better first #include <gambit.h> and then check that we've
  * got a known to be good version instead of unconditionally
  * overwriting it?
  */
-#define USE_EXPERIMENTAL 0
-#if (USE_EXPERIMENTAL = 0)
-#define ___VERSION 407009
-#else
-#define ___VERSION 409002
-#fi
 #include <gambit.h>
-#if (___VERSION < 409002 )
+
+#if (___VERSION < 409002)
 #define LINKER ____20_@SCM_LINKER@
 #else
 #define LINKER ___LNK_@SCM_LINKER@
-#fi
+#endif
+
 ___BEGIN_C_LINKAGE
 extern ___mod_or_lnk LINKER (___global_state_struct*);
 ___END_C_LINKAGE
@@ -45,11 +41,11 @@ void lambdanative_payload_setup()
   setup_params.debug_settings = debug_settings;
   ___setup(&setup_params);
   #if defined(ANDROID)
-  #if (___VERSION < 409002 )
-  ___disable_heartbeat_interrupts()
-  #else
-  ___cleanup_heartbeat_interrupt_handling();
-  #endif
+    #if (___VERSION < 409002)
+      ___disable_heartbeat_interrupts();
+    #else
+      ___cleanup_heartbeat_interrupt_handling();
+    #endif
   #endif
 }
 
