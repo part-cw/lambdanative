158c23d6cb43befa1b54ad04a42d56a0fe2e1356
Jörg F. Wittenberger <Joerg.Wittenberger@softeyes.net>
2019-05-23 16:09:07 +0200

    Update to recent gambit.
    Change USE_EXPERIMENTAL in languages/scm.c to switch between gambc
    4.7.9 and gambit 4.9.2.

--- languages/scm.c
+++ languages/scm.c
@@ -8,9 +8,23 @@
 #define DMSG(fmt...)
 #endif
 
+/*
+ * Maybe we better fist #include <gambit.h> and then check that we've
+ * got a known to be good version instead of unconditionally
+ * overwriting it?
+ */
+#define USE_EXPERIMENTAL 0
+#if (USE_EXPERIMENTAL = 0)
 #define ___VERSION 407009
+#else
+#define ___VERSION 409002
+#fi
 #include <gambit.h>
+#if (___VERSION < 409002 )
 #define LINKER ____20_@SCM_LINKER@
+#else
+#define LINKER ___LNK_@SCM_LINKER@
+#fi
 ___BEGIN_C_LINKAGE
 extern ___mod_or_lnk LINKER (___global_state_struct*);
 ___END_C_LINKAGE
@@ -31,7 +45,11 @@ void lambdanative_payload_setup()
   setup_params.debug_settings = debug_settings;
   ___setup(&setup_params);
   #if defined(ANDROID)
-  ___disable_heartbeat_interrupts();
+  #if (___VERSION < 409002 )
+  ___disable_heartbeat_interrupts()
+  #else
+  ___cleanup_heartbeat_interrupt_handling();
+  #endif
   #endif
 }
 
