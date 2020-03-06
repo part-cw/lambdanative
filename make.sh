part-cw/lambdanative

commit 2a9fbd4776efaca47e9f08ecb1316c10c1e1f908

MAKE: Avoid repeated xetex checks if working. Thanks @0-8-15

make.sh
@@ -1328,6 +1328,8 @@ END
   rm -rf $chkdir
   if [ $USE_XETEX = no ]; then
     echo " ** Using GD to render strings"
+  else
+    touch $SYS_TMPDIR/.use_xetex
   fi
 }
 
@@ -1348,7 +1350,7 @@ make_toolcheck()
   asserttool grep wget zip tar sed tr cut tail head find
   # language 
   asserttool autoconf make gcc patch
-  if [ `is_gui_app` = "yes" ]; then
+  if [ `is_gui_app` = "yes" -a ! -f $SYS_TMPDIR/.use_xetex ]; then
     make_xelatexcheck
   fi
   # platform specific tools

commit f1a71432430f27d34f8822fddb5ac546cf50bfc7

MAKE.SH Reinstate use of xelatex, which was broken in 2a9fbd4

make.sh
@@ -1357,6 +1357,9 @@ make_toolcheck()
   if [ `is_gui_app` = "yes" -a ! -f $SYS_TMPDIR/.use_xetex ]; then
     make_xelatexcheck
   fi
+  if [ -f $SYS_TMPDIR/.use_xetex ]; then
+    USE_XETEX=yes
+  fi
   # platform specific tools
   if [ -s targets/$SYS_PLATFORM/check-tools ]; then
     . targets/$SYS_PLATFORM/check-tools