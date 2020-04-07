d9d27a6f2603532cb40d4de9e9009cd85d26aa27
Matthias Görges <mgorges@cw.bc.ca>
2020-04-06 19:00:23 -0700

    ROTATION: Add ios code and rename variables to be consistent

--- modules/rotation/rotation.scm
+++ modules/rotation/rotation.scm
@@ -1,6 +1,6 @@
 #|
 LambdaNative - a cross-platform Scheme framework
-Copyright (c) 2009-2013, University of British Columbia
+Copyright (c) 2009-2020, University of British Columbia
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or
@@ -41,11 +41,10 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DArotE.
 (c-declare  #<<end-of-c-declare
 
 #ifdef IOS
-  double ios_rot_getq1(void);
-  double ios_rot_getq2(void);
-  double ios_rot_getq3(void);
-  double ios_rot_getq4(void);
-  double ios_rot_getaccuracy(void);
+  double ios_rot_getx(void);
+  double ios_rot_gety(void);
+  double ios_rot_getz(void);
+  double ios_rot_getw(void);
 #endif
 
 #ifdef ANDROID
@@ -57,58 +56,58 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DArotE.
 #endif
 
 
-static double rot_q1(void) {
+static double rotation_x(void) {
 #ifdef IOS
-  return 0; //ios_rot_getq1();
+  return ios_rot_getx();
 #elif ANDROID
   return android_rot_getq1();
 #else
   return 0.;
 #endif
 }
-static double rot_q2(void) {
+static double rotation_y(void) {
 #ifdef IOS
-  return 0; //ios_rot_getq2();
+  return ios_rot_gety();
 #elif ANDROID
   return android_rot_getq2();
 #else
   return 0.;
 #endif
 }
-static double rot_q3(void) {
+static double rotation_z(void) {
 #ifdef IOS
-  return 0; //ios_rot_getq3();
+  return ios_rot_getz();
 #elif ANDROID
   return android_rot_getq3();
 #else
   return 0.;
 #endif
-}      
-static double rot_q4(void) {
+}
+static double rotation_w(void) {
 #ifdef IOS
-  return 0; //ios_rot_getq4();
+  return ios_rot_getw();
 #elif ANDROID
   return android_rot_getq4();
 #else
   return 0.;
 #endif
-}             
+}
 static double rot_accuracy(void) {
 #ifdef IOS
-  return 0; //ios_rot_getaccuracy();
+  return -1;
 #elif ANDROID
   return android_rot_getaccuracy();
 #else
-  return 0.;
-#endif           
-}             
+  return -1;
+#endif
+}
 end-of-c-declare
 )
 
-(define rot-q1 (c-lambda () double "rot_q1")) ;;x*sin(θ/2)
-(define rot-q2 (c-lambda () double "rot_q2")) ;;[1]: y*sin(θ/2)
-(define rot-q3 (c-lambda () double "rot_q3")) ;;[2]: z*sin(θ/2)
-(define rot-q4 (c-lambda () double "rot_q4")) ;;[3]: cos(θ/2)
-(define rot-accuracy (c-lambda () double "rot_accuracy")) ;;[4]: estimated heading Accuracy (in radians) (-1 if unavailable)
+(define rotation-x (c-lambda () double "rotation_x")) ;;x*sin(θ/2)
+(define rotation-y (c-lambda () double "rotation_y")) ;;[1]: y*sin(θ/2)
+(define rotation-z (c-lambda () double "rotation_z")) ;;[2]: z*sin(θ/2)
+(define rotation-w (c-lambda () double "rotation_w")) ;;[3]: cos(θ/2)
+(define rotation-accuracy (c-lambda () double "rot_accuracy")) ;;[4]: estimated heading Accuracy (in radians) (-1 if unavailable)
 
 ;; eof
