ce5e6343fc08764351de43d07d411a6a531b34a0
karliwalti <dev@karliwalti.ch>
2020-09-27 16:23:10 +0200

    [eventloop] adds backward compatible option to disable mediascanner on android

--- modules/eventloop/eventloop.scm
+++ modules/eventloop/eventloop.scm
@@ -256,12 +256,13 @@ end-of-c-declare
 ;; override gambit main definition for backwards compatibility
 (set! main ln-main)
 
-(define (terminate)
+(define (terminate . nomediascanner)
   (if app:android? (begin
-    (android-run-mediascanner)
-    (android-finish)
-    (let loop ()
-      (if (not android-mediascanner-done?) (begin (thread-sleep! 0.1) (loop))))))
+    (let ((nm (not (if (= (length nomediascanner) 1) (car nomediascanner) #f))))
+     (if nm (android-run-mediascanner))
+     (android-finish)
+     (if nm (let loop ()
+      (if (not android-mediascanner-done?) (begin (thread-sleep! 0.1) (loop))))))))
   (if (procedure? hook:terminate) (hook:terminate))
 )
 
