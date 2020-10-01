part-cw/lambdanative

commit c851804a1ef4abac52cfd08e7756b7e8c84ad51d

UIFORM small bug fixes
thanks for adding the slider feature as proposed.
this fixes two bugs. 
1) makes test if slider has been activated functional (otherwise no input would add defaultvalue to field)
2) removes crash when incorrect label is passed (eg only one string in list)

modules/uiform/uiform.scm
@@ -1418,7 +1418,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
          (stepnum (fx- max min))
          (stepvalues  (make-list-natural min (+ 1 stepnum)))
          (defaultvalue (glgui:uiform-arg args 'default (/ stepnum 2)))
-         (value (xxget loc id defaultvalue))
+         (value (xxget loc id #f))
          (boxcolor (uiget 'color-default)))
      (uiset idvalues stepvalues)
      (if req
@@ -1430,18 +1430,20 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
               (bh bw)
               (bx (+ x (* (/ sw stepnum) (- value min)) (- (* w 0.1) (/ bh 2))))
               (by y)
+	      (v (if value value defaultvalue))
               (i (/ sw stepnum))
               (positions (make-list-increment (* w 0.1) (+ 1 stepnum) i)))
 	     (uiset idpositions positions)
          (glgui:draw-box (+ x (* w 0.1)) (+ by (/ bh 4)) sw (/ bh 2) boxcolor) ;; Horizontal bar
-	     (glgui:draw-box bx by bw bh (if (null? value) boxcolor White))  ;; Slider box
-         (if shownumber
-           (glgui:draw-text-center bx by bw bh (number->string value) fnt Black))
-         ;; draw labels
+	     (glgui:draw-box bx by bw bh (if value boxcolor White))  ;; Slider box
+         (if (and shownumber value)
+           (glgui:draw-text-center bx by bw bh (number->string v) fnt Black))
+         ;; draw labels if set
+	 (if (fx> (length labels) 1) (begin  
          (glgui:draw-text-left (+ x (* w 0.1)) (+ by (- h fnth)) (- (* w 0.8) bw) fnth (car labels) fnt White)
          (glgui:draw-text-right (+ x bw (* w 0.1)) (+ by (- h fnth)) (- (* w 0.8) bw) fnth (car (reverse labels)) fnt White)
          (if (> (length labels) 2)
-           (glgui:draw-text-center (+ x (/ sw 2) (- (* w 0.1) (/ (- (* w 0.8) bw) 2))) (+ by (- h fnth)) (- (* w 0.8) bw) fnth (cadr  labels) fnt White))))
+           (glgui:draw-text-center (+ x (/ sw 2) (- (* w 0.1) (/ (- (* w 0.8) bw) 2))) (+ by (- h fnth)) (- (* w 0.8) bw) fnth (cadr  labels) fnt White)))))
       h
   ))
 

commit a43b032781b128117a8690d0d5381b53bf607bfd

UIFORM: Fix bug in glgui:uiform-slider-draw introduced in c851804
Error came in #208, whereby value was used but now initialized
with #f and the new variable v which has a default was set below

modules/uiform/uiform.scm
@@ -1507,17 +1507,17 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
        (uiform-required-set id (abs (- (abs y) (uiget 'offset 0) h))))
      (if (uiget 'sanemap)
        (let* ((fnth (glgui:fontheight fnt))
+              (v (if value value defaultvalue))
               (sw (* w 0.8))
               (bw (- h fnth))
               (bh bw)
-              (bx (+ x (* (/ sw stepnum) (- value min)) (- (* w 0.1) (/ bh 2))))
+              (bx (+ x (* (/ sw stepnum) (- v min)) (- (* w 0.1) (/ bh 2))))
               (by y)
-	      (v (if value value defaultvalue))
               (i (/ sw stepnum))
               (positions (make-list-increment (* w 0.1) (+ 1 stepnum) i)))
 	     (uiset idpositions positions)
          (glgui:draw-box (+ x (* w 0.1)) (+ by (/ bh 4)) sw (/ bh 2) boxcolor) ;; Horizontal bar
-	     (glgui:draw-box bx by bw bh (if value White boxcolor))  ;; Slider box
+         (glgui:draw-box bx by bw bh (if value White boxcolor))  ;; Slider box
          (if (and shownumber value)
            (glgui:draw-text-center bx by bw bh (number->string v) fnt Black))
          ;; draw labels if set