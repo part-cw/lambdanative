9bc9dc6d456d4d0e8f62363e48cda3417d1d7e8b
0-8-15 <Joerg.Wittenberger@softeyes.net>
2020-10-02 01:14:04 +0200

    SCM: in debug mode: enable more debugging (#342)
    Remove gambit optimizations (block) and (not safe) in debug mode. Note: This will increase compile time when running in debug mode.

--- languages/scm.sh
+++ languages/scm.sh
@@ -52,7 +52,7 @@ compile_payload_scm()
     # -------
     # prep the compiler options
     if [ $SYS_MODE = "debug" ]; then
-      scm_opts="(declare (block)(not safe)(standard-bindings)(extended-bindings)(debug)(debug-location))"
+      scm_opts="(declare (standard-bindings)(extended-bindings)(debug)(debug-location))"
     else
       scm_opts="(declare (block)(not safe)(standard-bindings)(extended-bindings))"
     fi
