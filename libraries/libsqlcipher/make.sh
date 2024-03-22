5193cdd8d69cfe2b7432fa7755498a0550af66ae
Matthias Görges <mgorges@bcchr.ca>
2024-03-21 19:54:45 -0700

    LIBSQLCIPHER: Make build on android

--- libraries/libsqlcipher/make.sh
+++ libraries/libsqlcipher/make.sh
@@ -6,7 +6,30 @@ package_download $PKGURL $PKGHASH
 package_patch
 rmifexists $SYS_PREFIX/include/sqlite3*.h
 
-package_configure --disable-tcl --disable-shared --enable-tempstore=always CFLAGS="-DSQLITE_HAS_CODEC"
+case $SYS_PLATFORM in
+ios)
+  EXTRACONF=--host=x86_64-apple-darwin
+;;
+android)
+  EXTRACONF=--host=arm-eabi
+  export LIBS='-llog'
+;;
+win32*)
+  EXTRACONF=--host=mingw32
+;;
+linux)
+  EXTRACONF=--host=i386-linux
+;;
+*)
+  EXTRACONF=
+;;
+esac
+
+if [ "$SYS_PLATFORM" = "$SYS_HOSTPLATFORM" ]; then
+  EXTRACONF=
+fi
+
+package_configure --disable-tcl --disable-shared --enable-tempstore=always CFLAGS="-DSQLITE_HAS_CODEC" $EXTRACONF
 
 package_make
 package_make install
