fa348e0f8659e0b3d2f694024d9d253b299d4c14
Benson Muite <bkmgit@users.noreply.github.com>
2026-05-29 19:05:00 +0300

    Use a release version of genann

--- libraries/libgenann/make.sh
+++ libraries/libgenann/make.sh
@@ -1,6 +1,7 @@
-PKGURL=https://github.com/codeplea/genann.git
+PKGURL=https://github.com/codeplea/genann/archive/refs/tags/v1.0.0.tar.gz
+PKGHASH=d8547e43c135cb85a88b5de6208e352852232500
 
-package_download $PKGURL
+package_download $PKGURL $PKGHASH
 
 rm *.o 2> /dev/null
 veval "$SYS_CC -c genann.c -o genann.o"
