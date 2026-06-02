b801cba6cce5c04aa75850306487bfb717af75b7
Matthias Görges <mgoerges@bcchr.ca>
2026-06-01 20:02:19 -0700

    MACOSX: Improve building for MacOS 26 to allow app debugging

--- modules/ln_glcore/glcore-ffi.scm
+++ modules/ln_glcore/glcore-ffi.scm
@@ -45,7 +45,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifdef MACOSX
 #include <Carbon/Carbon.h>
 #include <OpenGL/OpenGL.h>
-#include <AGL/agl.h>
+#include <OpenGL/gl.h>
 #endif
 
 #if defined(IOS)
