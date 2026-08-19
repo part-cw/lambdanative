d936db275b8f40c9e1381783df03c8981ecc5481
Benson Muite <bkmgit@users.noreply.github.com>
2026-08-19 10:19:24 +0300

    Download freetype from GNU mirror
    Reduce load on GNU server

--- libraries/libfreetype/make.sh
+++ libraries/libfreetype/make.sh
@@ -1,4 +1,4 @@
-PKGURL=https://download.savannah.gnu.org/releases/freetype/freetype-2.10.1.tar.gz
+PKGURL=https://download-mirror.savannah.gnu.org/releases/freetype/freetype-2.10.1.tar.gz
 PKGHASH=3296b64ad1e7540289f22e4b6383e26e928b0a20
 
 package_download $PKGURL $PKGHASH
